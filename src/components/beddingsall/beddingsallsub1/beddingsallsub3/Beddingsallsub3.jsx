85616f6a2bbf231081fc0c234f8770d31b24efd4
chukaconnect <chukaconnect@gmail.com>
2023-03-24 09:40:50 +0300

    headers

--- src/components/beddingsall/beddingsallsub1/beddingsallsub3/Beddingsallsub3.jsx
+++ src/components/beddingsall/beddingsallsub1/beddingsallsub3/Beddingsallsub3.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import {useNavigate} from 'react-router-dom'
 import './all.css'
-import Header from "../../../common/header/Header"
+import Header from "../../../../common/header/Header"
 
 const Beddingsallsub3 = ({ beddingsItems,CartItem,productItems}) => {
 
